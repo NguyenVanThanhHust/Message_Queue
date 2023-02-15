46bf9951c751698d2041e1c0af229c879136fc79
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-02-15 16:52:18 +0700

    temp commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://codepr.github.io/posts/sol-mqtt-broker/
---
+++ src/mqtt.hpp
@@ -0,0 +1,14 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+#include <stdio.h>
+
+#define MQTT_HEADER_LEN 2
+#define MQTT_ACK_LEN    4
+
+/*
+Define first byte in fixed header
+*/
+
+
+#endif
